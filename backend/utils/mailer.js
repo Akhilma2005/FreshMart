7364d855803cd37ab19e3ac9fdbf8e56a4c325be
Akhilma2005 <maakhil432005@gmail.com>
2026-03-23 21:42:27 +0530

    fix: switch from Gmail SMTP to Resend HTTP API (Render blocks SMTP port 465)

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,21 +1,17 @@
-const nodemailer = require('nodemailer');
-
-const getTransporter = () => nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
-  },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
-});
+const { Resend } = require('resend');
 
 module.exports = {
-  sendMail: (options) => {
-    if (!process.env.MAIL_USER || !process.env.MAIL_PASS) {
-      return Promise.reject(new Error('MAIL_USER and MAIL_PASS env vars are not set on the server.'));
-    }
-    return getTransporter().sendMail(options);
+  sendMail: async ({ to, subject, html }) => {
+    const apiKey = process.env.RESEND_API_KEY;
+    if (!apiKey) throw new Error('RESEND_API_KEY env var is not set on the server.');
+
+    const resend = new Resend(apiKey);
+    const { error } = await resend.emails.send({
+      from: 'FreshMart <onboarding@resend.dev>',
+      to,
+      subject,
+      html,
+    });
+    if (error) throw new Error(error.message);
   },
 };
